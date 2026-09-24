985ed6119205688d9a49576cbd38059751b83a7e
aquanubandh <aquanubandh@gmail.com>
2026-09-24 20:15:53 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,9 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://aquanubandh.com/sitemap.xml",
+    sitemap: [
+      "https://aquanubandh.com/sitemap.xml",
+      "https://lab.aquanubandh.com/sitemap.xml",
+    ],
   };
 }
